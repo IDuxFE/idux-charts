2be44c0c486f42f46504f2ca05a2e11baaeb246b
何锦辉99460 <99460@sangfor.com>
2023-08-29 16:21:33 +0800

    feat: 补充平滑面积图，并调整一下渐变颜色。

---
+++ packages/site/src/components/line/demos/NoAxis.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { type LineChartProps, useLinearGradient } from '@idux/charts'
+
+const data = [123, 324, 156, 244, 188]
+
+const lineOption: LineChartProps = {
+  smooth: true,
+  lineStyle: {
+    shadowColor: 'rgba(69, 143, 255, 0.8)',
+    shadowBlur: 10,
+  },
+  areaStyle: {
+    color: useLinearGradient(),
+  },
+  title: { text: '折线图标题' },
+  xAxis: {
+    show: false,
+    data: ['星期一', '星期二', '星期三', '星期四', '星期五'],
+  },
+  yAxis: {
+    show: false,
+    name: '次',
+  },
+}
+</script>
+<template>
+  <IxLineChart style="height: 300px" :data="data" v-bind="lineOption" />
+</template>
+
+<archive-meta lang="json">
+{
+  "title": "无坐标轴",
+  "description": "",
+  "index": 9
+}
+</archive-meta>
---
+++ packages/site/src/components/line/demos/SmoothAreaLine.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { type LineChartProps, useLinearGradient } from '@idux/charts'
+
+const data = [123, 324, 156, 244, 188]
+
+const lineOption: LineChartProps = {
+  smooth: true,
+  lineStyle: {
+    shadowColor: 'rgba(69, 143, 255, 0.35)',
+    shadowBlur: 10,
+  },
+  areaStyle: {
+    color: useLinearGradient(),
+  },
+  title: { text: '折线图标题' },
+  xAxis: {
+    data: ['星期一', '星期二', '星期三', '星期四', '星期五'],
+  },
+  yAxis: {
+    name: '次',
+  },
+  onReady (echartsInstance) {
+    console.log('图彪实例：', echartsInstance);
+  }
+}
+
+</script>
+<template>
+  <IxLineChart style="height: 300px" :data="data" v-bind="lineOption" />
+</template>
+
+<archive-meta lang="json">
+{
+  "title": "平滑面积图",
+  "description": "",
+  "index": 9
+}
+</archive-meta>
